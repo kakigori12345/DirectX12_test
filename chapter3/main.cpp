7f56eacb8733743020148b4114c1bc9bcbe12ba6
Kaigori123 <e1212@ob.akashi.ac.jp>
2021-12-13 23:56:18 +0900

    ウィンドウ生成関数追加

--- chapter3/main.cpp
+++ chapter3/main.cpp
@@ -12,16 +12,60 @@ void DebugOutputFormatString( const char* format, ...) {
 #ifdef _DEBUG
 	va_list valist;
 	va_start( valist, format);
-	printf( format, valist);
+	vprintf( format, valist);
 	va_end(valist);
 #endif
 }
+
+// 面倒 だ けど 書か なけれ ば いけ ない 関数
+LRESULT WindowProcedure( HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	// ウィンドウ が 破棄 さ れ たら 呼ば れる
+	if (msg == WM_DESTROY) { PostQuitMessage( 0);
+	// OS に対して「 もうこ の アプリ は 終わる」 と 伝える
+	return 0;
+	}
+	
+	return DefWindowProc(hwnd, msg, wparam, lparam); // 既定 の 処理 を 行う
+}
+
+
 
 #ifdef _DEBUG
 int main() {
 #else 
 int WINAPI WinMain( HINSTANCE, HINSTANCE, LPSTR, int) {
 #endif
+	// ウィンドウ クラス の 生成＆ 登録
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof( WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure; // コール バック 関数 の 指定
+	w.lpszClassName = _T("DX12Sample"); // アプリケーション クラス 名（ 適当 で よい）
+	w.hInstance = GetModuleHandle(nullptr); // ハンドル の 取得
+	RegisterClassEx(&w); // アプリケーション クラス（ ウィンドウ クラス の 指定 を OS に 伝える）
+	RECT wrc = { 0, 0, window_width, window_height};// ウィンドウサイズ を 決める
+													 
+	// 関数 を 使っ て ウィンドウ の サイズ を 補正 する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウ オブジェクト の 生成
+	HWND hwnd = CreateWindow(
+		w. lpszClassName,// クラス 名 指定
+		_T("DX12テスト"), // タイトル バー の 文字
+		WS_OVERLAPPEDWINDOW, // タイトル バー と 境界線 が ある ウィンドウ
+		CW_USEDEFAULT, // 表示 x 座標 は OS に お 任せ
+		CW_USEDEFAULT, // 表示 y 座標 は OS に お 任せ
+		wrc.right - wrc.left, // ウィンドウ 幅
+		wrc.bottom - wrc.top, // ウィンドウ 高
+		nullptr, // 親 ウィンドウ ハンドル
+		nullptr, // メニュー ハンドル
+		w.hInstance, // 呼び出し アプリケーション ハンドル
+		nullptr); // 追加 パラメーター
+	
+	// ウィンドウ 表示
+	ShowWindow( hwnd, SW_SHOW);
+
+
 	DebugOutputFormatString(" Show window test.");
 	getchar();
 	return 0;
